c4396e6b0e2035bfaa36857199653c1ab354f3e1
AdiD2021 <>
2021-06-28 16:25:58 +0000

    Added code to display escape characters

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,6 @@
+#Display escape characters
+print("\n Display a new line")
+print("\t Display a tap space")
+print("\\ Display a back slash")
+print ("\` Display a single quate")
+print("\`` Display double quote")
