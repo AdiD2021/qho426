69bebab62635a8326f1cece002c043c7fdf745fd
AdiD2021 <>
2021-06-30 16:08:37 +0000

    not gooing great

---
+++ basics/input/string_operators.py
@@ -0,0 +1,8 @@
+print("please enter the number of lives")
+whole_number = int(input(3))
+print("Please enter the energy level")
+whole_number = int(input(10))
+print("please enter the shield level")
+whole_number = int(input(7))
+print("Health has been set.")
+print("Lives  ")
