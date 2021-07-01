820285e8c4bb02eaffed6e8e559100db18f81040
AdiD2021 <>
2021-07-01 07:37:03 +0000

    a lot

---
+++ basics/input/review.py
@@ -0,0 +1,14 @@
+print("Hey Hey")
+# This is a comment
+print("Great Day!")
+#Display message to the standard output
+print ("System Failure Imminent!")
+print("System reboot has been initiated...")
+print("...rebooting now")
+print("emoji")
+print("System online")
+print("##########")
+print("#        #")
+print("#        #")
+print("#        #")
+print("##########")
